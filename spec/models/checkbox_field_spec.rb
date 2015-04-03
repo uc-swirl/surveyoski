181d77be6ee595df6eefa314298be38b27b185b7
Marco Montagna <marcojoemontagna@gmail.com>
2015-04-03 02:12:33 -0700

    Added tests for checkboxs

--- spec/models/checkbox_field_spec.rb
+++ spec/models/checkbox_field_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
 describe CheckboxField do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @check = CheckboxField.new(:question_title => "Waiting", :question_description => "Between worlds")
+  end
+
+  it "store has a title" do
+    expect(@check.question_title).to eq("Waiting")
+  end
+  it "store has a title" do
+    expect(@check.question_description).to eq("Between worlds")
+  end
+
+  it "parses it's options" do
+    @check.parse_options("A:1\r\nB:2\r\nC:3")
+
+    expect(@check.field_options).to eq([["A", "1"], ["B", "2"], ["C", "3"]])
+  end
+
+  it "has a nice name" do
+    expect(CheckboxField.nice_name).to eq("Checkbox")
+  end
 end
