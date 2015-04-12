3d369ff9ab96ac403c2e74a0e07e1055f2daa32b
Marco Montagna <marcojoemontagna@gmail.com>
2015-04-11 22:05:20 -0700

    Added required field to SurveyFields

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150401181311) do
+ActiveRecord::Schema.define(:version => 20150412050307) do
 
   create_table "field_responses", :force => true do |t|
     t.string   "response"
@@ -46,6 +46,7 @@ ActiveRecord::Schema.define(:version => 20150401181311) do
     t.string   "radio_button_options"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
+    t.boolean  "required"
   end
 
   create_table "survey_templates", :force => true do |t|
