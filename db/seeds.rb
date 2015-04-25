9f231c00bad12afa599a568867667229a4c7d3a0
Marco Montagna <marcojoemontagna@gmail.com>
2015-04-25 12:03:41 -0700

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 
 @course = Course.create :name => "Example Course"
 
-@user = User.create :email => "mmontagna@berkeley.edu", :status => "admin"
+@user = User.create :email => "mmontagna@berkeley.edu", :name => "Marco", :status => "admin"
 
 
 @template = SurveyTemplate.new :survey_title => "Example Survey"
@@ -10,10 +10,14 @@
 @template.user = @user
 @template.course = @course
 
+@check = CheckboxField.create(:question_title => "Example Question", :field_options => [["one1", "1"], ["two2", "2"]])
 
-@template.survey_fields << CheckboxField.create :field_options => [["one1", "1"]. ["two2", "2"]]
 
-@template.save!
+@template.survey_fields << @check
+
+@text = TextQuestionField.create(:question_title => "Example Text")
+@template.survey_fields << @text
+
 
 @enrollment = @user.enrollments.build 
 
@@ -21,4 +25,21 @@
 @enrollment.course = @course
 
 
+
+
+@submission = Submission.create 
+@submission.survey_template = @template
+
+@submission.field_responses << @check.field_responses.build :response => "1"
+
+@submission.field_responses << @text.field_responses.build :response => "Text response"
+
 @user.save!
+
+@template.save!
+
+
+
+
+
+
