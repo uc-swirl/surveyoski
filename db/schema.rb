6692cf95312a8aa71e0eaeb19002b88fc5f41769
Marco Montagna <marcojoemontagna@gmail.com>
2015-04-25 13:19:21 -0700

    Bootstrap

--- db/schema.rb
+++ db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(:version => 20150425002905) do
     t.string   "question_title"
     t.string   "question_description"
     t.string   "field_options"
-    t.string   "survey_fields"
     t.datetime "created_at",           :null => false
     t.datetime "updated_at",           :null => false
     t.boolean  "required"
