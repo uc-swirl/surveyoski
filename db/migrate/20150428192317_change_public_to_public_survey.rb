12250b05efa794bf09b7d3eff1e41753373f68fa
Alex <alexlily@berkeley.edu>
2015-05-02 04:34:26 -0300

    fix migration

--- db/migrate/20150428192317_change_public_to_public_survey.rb
+++ db/migrate/20150428192317_change_public_to_public_survey.rb
@@ -1,6 +1,6 @@
 class ChangePublicToPublicSurvey < ActiveRecord::Migration
   def up
-  	remove_column :survey_templates, :public, :boolean
+  	remove_column :survey_templates, :public
   	add_column :survey_templates, :public_survey, :boolean
   end
 end
